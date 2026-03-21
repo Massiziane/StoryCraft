f7a582797d822718f680508f8d77361bc5dd6d76
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-03-21 17:03:17 -0400

    feat: load .env into typed settings object

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,3 @@ app.add_middleware(
 if __name__ == "__main__":
     import uvicorn # webserver to serve FastAPI application
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
-
-
