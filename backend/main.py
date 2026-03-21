1ae7cbedc9c19ebcd29887372ee61ab643558fb2
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-03-21 16:51:56 -0400

    feat: initialize FastAPI app with CORS middleware and uvicorn webserver

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+# Create FastAPI app
+app = FastAPI(
+    title="StoryCraft API",
+    description="API to generate stories",
+    version="0.1.0",
+    docs_url="/docs", 
+    redoc_url="/redoc" 
+    )
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )   
 
 if __name__ == "__main__":
-    main()
+    import uvicorn # webserver to serve FastAPI application
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+
