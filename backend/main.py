674ab502f1f68b73888ae03ef9afe09f27b12897
Massiziane <e2185817@cmaisonneuve.qc.ca>
2026-03-29 12:48:43 -0400

    routes : connecting endpoints to main

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from core.config import settings
+
+from routers import story, job
 
 
 # Create FastAPI app
@@ -20,6 +23,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
     )   
+
+app.include_router(story.router, prefix=settings.API_PREFIX)
+app.include_router(job.router, prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     import uvicorn # webserver to serve FastAPI application
